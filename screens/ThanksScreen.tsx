1632ed6771ef2c80a0bcb50252fe00df2a0dcc4a
nazrul <nazrulwo@gmail.com>
2025-03-12 10:51:55 +0100

    Fix the leave page

--- screens/ThanksScreen.tsx
+++ screens/ThanksScreen.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import styles from '../styles/GlobalStyles';
 
+type RootStackParamList = {
+  Home: undefined;
+  Leave: undefined;
+  Thanks: undefined;
+};
+
 const ThanksScreen: React.FC = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('Home');
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Thank you for visiting Softcode! </Text>
-      <Text style={styles.subText}>Have a great day!</Text>
+      <Text style={styles.text}>Thank you for visiting!</Text>
+      <Text style={styles.subText}>We hope to see you again soon.</Text>
     </View>
   );
 };
